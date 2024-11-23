806d188a3cee4f1fa38e7345554c82bddcb8b83b
Jessy E Street <jessica.street12@gmail.com>
2024-11-23 03:26:49 +0000

    Final year project
    Third Person adventure, puzzle, narrative game, based in the 70's you're a female detective trying to solve your sister murder.

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+
+
+    private float moveSpeed;
+    public float walkSpeed;
+    public float sprintSpeed;
+
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+    public Transform cam;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+
+
+
+
+
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    void Update()
+    {
+        if (Input.GetKey(sprintKey))
+        {
+            moveSpeed = sprintSpeed;
+
+
+        }
+
+        else
+        {
+            moveSpeed = walkSpeed;
+        }
+
+
+
+        //Rotation Code From Link //https://www.youtube.com/watch?v=4HpC--2iowE 
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * moveSpeed * Time.deltaTime);
+        }
+
+
+
+
+    }
+}
