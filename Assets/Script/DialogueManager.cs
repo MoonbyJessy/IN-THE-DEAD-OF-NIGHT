eb1cc5e81ed736b455776f45d7f7794d42530e27
Jessy E Street <jessica.street12@gmail.com>
2025-01-23 13:34:30 +0000

    yes

--- Assets/Script/DialogueManager.cs
+++ Assets/Script/DialogueManager.cs
@@ -1,17 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
+    public Text nameText;
+    public Text dialogueText;
+
     private Queue<string> sentences;
     void Start()
     {
         sentences = new Queue<string>();
     }
+
+    public Text GetNameText()
+    {
+        return nameText;
+    }
+
     public void StartDialogue (Dialogue dialogue)
     {
-        Debug.Log("Starting conversation with " + dialogue.name);
+        nameText.text = dialogue.name;
         sentences.Clear();
 
         foreach (string sentence in dialogue.sentences)
@@ -23,7 +33,17 @@ public class DialogueManager : MonoBehaviour
     }
     public void DisplayNextSentence ()
     {
-
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+    public void EndDialogue ()
+    {
+        Debug.Log("End of Converstation.");
     }
     
 }
